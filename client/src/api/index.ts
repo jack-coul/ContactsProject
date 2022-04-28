22b71d6067c679a821c3e318b59ac744f4403de7
Islam <buffon0766@gmail.com>
2022-04-28 16:28:53 +0300

    final

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -2,19 +2,19 @@ import axios from "axios";
 
 export const API = {
   get: async (url: string) => {
-    const res = await axios.get(`/api/v1/${url}`);
+    const res = await axios.get(`http://localhost:4000/api/v1/${url}`);
     return res;
   },
   post: async (url: string, data: object) => {
-    const res = await axios.post(`/api/v1/${url}`, data);
+    const res = await axios.post(`http://localhost:4000/api/v1/${url}`, data);
     return res;
   },
   patch: async (url: string, data: object) => {
-    const res = await axios.patch(`/api/v1/${url}`, data);
+    const res = await axios.patch(`http://localhost:4000/api/v1/${url}`, data);
     return res;
   },
   delete: async (url: string) => {
-    const res = await axios.delete(`/api/v1/${url}`);
+    const res = await axios.delete(`http://localhost:4000/api/v1/${url}`);
     return res;
   },
 };
